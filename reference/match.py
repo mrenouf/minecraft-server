8878e83a482e02439edf7c483486f359dfbf973d
tweakt <mark@bitgrind.com>
2010-11-15 21:57:00 -0500

    Updated the python matcher to be multithreaded.
    Now completes in 25s, compared to 1m 20s for the bash version.
    The initial pass is still an O(n^2) all pairs comparison between
    the two sets but it's done in parallel by N-threads.
    
    Then cases of duplicate matches are resolved by taking the best
    match for the target (the bash script didn't do this).
    
    Finally, the list is sorted by best match first.

--- reference/match.py
+++ reference/match.py
@@ -3,13 +3,23 @@
 import sys
 import os
 import subprocess
+import threading,Queue,time,sys,traceback
+from timeit import itertools
+from operator import itemgetter
 
 # Some tuneable parameters for simhash
 feature_count=1024
 shingle_size=4
 
+# Number of threads for initial scoring
+num_worker_threads=6
+
+scores={}
+
 def get_files(d):
-  return filter(lambda f: not f.endswith('.sim'), os.listdir(d))
+  return filter(lambda f: not os.path.isdir(os.path.join(d, f)) \
+      and not f.endswith('.sim') and os.path.getsize(os.path.join(d, f)) > 0, \
+      os.listdir(d))
 
 def gen_hash(path, files):
   args = ['simhash', '-f', str(feature_count), '-s', str(shingle_size), '-w']
@@ -18,48 +28,68 @@ def gen_hash(path, files):
 
 def score(group1_file, group2_file):
   p = subprocess.Popen(['simhash', '-c', group1_file + '.sim', group2_file + '.sim'], \
-	stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  	stdout=subprocess.PIPE)
   p.wait()
   score = p.stdout.readline().strip()
-  if len(score) is 0:
-    return 0
-  return float(score)
+  if len(score) > 0:
+    score = float(score)
+  return score
+
+def git_rename():
+    return
+
+def build_scores():
+  global scores
+  while True:
+    f1 = q.get()
+    scores[f1] = max(itertools.imap(lambda f2: \
+        (f2, score(os.path.join(g1_dir, f1), os.path.join(g2_dir, f2))), g2), \
+        key=lambda i: i[1])
+    q.task_done()
+
 
 def main(args=sys.argv):
-  (group1_dir, group2_dir) = args[1:3] 
+  global q, g1_dir, g2_dir, g1, g2, scores
+  (g1_dir, g2_dir) = args[1:3] 
 
-  if not (os.path.exists(group1_dir) or os.path.exists(group2_dir)):
+  if not (os.path.exists(g1_dir) or os.path.exists(g2_dir)):
     print "Usage <group1_dir> <group2_dir>"
     return 1
 
-  group1 = get_files(group1_dir)
-  gen_hash(group1_dir, group1)
+  g1 = get_files(g1_dir)
+  gen_hash(g1_dir, g1)
 
-  group2 = get_files(group2_dir)
-  gen_hash(group2_dir, group2)
+  g2 = get_files(g2_dir)
+  gen_hash(g2_dir, g2)
 
+  q = Queue.Queue()
+  for i in range(num_worker_threads):
+    t = threading.Thread(target=build_scores)
+    t.daemon = True
+    t.start()
+
+  # queue up all files in group1 and wait
+  for f1 in g1: 
+    q.put(f1)
+  q.join()
+  
+  # build an inverse map
   matches={}
+  for f1 in g1:
+    (f2, score) = scores[f1]
+    matches[f2] = (f1, score)
 
-  for group1_file in group1:
-    best=""
-    best_score = 0
-    for group2_file in group2:
-      file_score = score(os.path.join(group1_dir, group1_file), \
-	os.path.join(group2_dir, group2_file))
-      if file_score > best_score:
-        best = group2_file
-        best_score = file_score
+  # join this with the forward map, taking the best overall match
+  for f1 in g1:
+    (f2, score) = scores[f1]
+    if (score > matches[f2][1]):
+      matches[f2] = (f1, score)
 
-    print "%s %s %2.2f" % (group1_file, best, best_score)
-    if matches.has_key(best) and best_score > matches[best]['score']:
-      orig = matches[best]['match']
-      print "%s: %s replaces %s (%2.3f => %2.3f)" % (group1_file, orig, best, matches[best]['score'], best_score)
-      matches[best] = {'match': group1_file, 'score': best_score};
-    elif not matches.has_key(best):
-      matches[best] = {'match': group1_file, 'score': best_score};
-    
-  print "------------------"
-  print "\n".join(map(lambda k: "%s %s %2.2f" % (matches[k]['match'], k, matches[k]['score']), sorted(matches)))
+  # flatten into list of tuples
+  result = [(matches[f2][0], f2, matches[f2][1]) for f2 in matches.keys()]
+
+  # sort, format and output the results
+  print "\n".join(map(lambda e: "%s %s %2.2f" % e, sorted(result, key=itemgetter(2), reverse=True)))
 
 if __name__=="__main__":
   sys.exit(main())
